0b93ea98d423a8443b6e2192345a6d76988cc46d
Felix Soubelet <felix.soubelet@protonmail.com>
2022-03-02 14:44:02 +0100

    don't forget betabeat columns

--- rws/constants.py
+++ rws/constants.py
@@ -15,3 +15,4 @@ VARIED_IR_QUADRUPOLES: List[int] = list(range(4, 11))
 EXPORT_TWISS_COLUMNS: List[str] = [colname.upper() for colname in MONITOR_TWISS_COLUMNS]
 EXPORT_TWISS_COLUMNS.remove("DBX")
 EXPORT_TWISS_COLUMNS.remove("DBY")
+EXPORT_TWISS_COLUMNS += ["BBX", "BBY"]  # beta-beat columns
